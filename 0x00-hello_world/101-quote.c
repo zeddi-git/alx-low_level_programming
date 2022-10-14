4b19761264c3708fbceb56b3b38dcc01279b68ff
Zeddi_Owino <zowino3041@gmail.com>
2022-10-14 09:47:38 -0700

    quote- UNIX is basically a simple operating system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - this is the Entry point
+ * Return: Always 1 (success)
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
