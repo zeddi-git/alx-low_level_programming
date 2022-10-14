7d143de2b66471f295925a7c07c87a67d0418d9c
Zeddi_Owino <zowino3041@gmail.com>
2022-10-14 09:15:39 -0700

    Size is not grandeur-printing the size of various types on the computer

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdiio.h>
+
+/**
+ * main - This is the entry point
+ * Return: Always zero (success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int l;
+	long long int x;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(x));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}
