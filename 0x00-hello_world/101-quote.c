a8b72615ea998830fb3a98bd3fb3c422db363e7a
Zeddi_Owino <zowino3041@gmail.com>
2022-10-14 10:05:31 -0700

    101 quote- using a new library called unistd

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
- * main - this is the Entry point
+ * main - this prints out output and that piece of art...
  * Return: Always 1 (success)
  */
+
 int main(void)
 {
-	fwrite(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
