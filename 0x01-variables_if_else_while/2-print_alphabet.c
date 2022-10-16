dbd23f8cc1d17f315133d839a625aefcb33c2e4c
Zeddi_Owino <zowino3041@gmail.com>
2022-10-16 12:54:19 -0700

    print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - This is the entry point
+ * Return: Always is zero (success)
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		putchar(low);
+		putchar('\n');
+	}
+	return (0);
+}
