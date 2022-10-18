11e8ebb28c8bcab5c6865112952590ebd0055fdb
Zeddi_Owino <zowino3041@gmail.com>
2022-10-18 08:01:31 -0700

    alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,15 +7,12 @@
  */
 int _isalpha(int c)
 {
-	char lower, upper;
-	int isletter = 0;
-
-	for (lower = 'a'; lower <= 'z'; lower++)
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
 	{
-		if (c == lower || c == upper)
-		{
-			isletter = 1;
-		}
+		return (1);
 	}
-	return (isletter);
+	else
+	{
+		return (0);
+	}
 }
