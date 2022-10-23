a524c4b11e78b6c8bacc18a2b2f96bb1fbadff7a
Zeddi_Owino <zowino3041@gmail.com>
2022-10-23 03:10:18 -0700

    test for negative and positive integer

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests the functions prints if integer is +ve or -ve
+ * Return: always 0
+ */
+int main (void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+We will learn about how to debug ouur codes and the methods we can use to debug our codes.
+One of the methods we have learnt is the Rubber Duck debugging
