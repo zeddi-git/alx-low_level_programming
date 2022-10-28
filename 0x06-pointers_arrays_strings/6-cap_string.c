5b2ac8d00b55bb0281b244d141450eec07b1ce16
Zeddi_Owino <zowino3041@gmail.com>
2022-10-28 03:26:30 -0700

    capitalizing

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * cap_string - funtion that capital string
+ * @s: Address of the string
+ * Return: The uppercase @s address
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			if ((s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n' || s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' || s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' || s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' || s[i - 1] == '}'))
+			{
+				s[i] -= 32;
+			}
+		}
+	}
+
+	return s;
+}
