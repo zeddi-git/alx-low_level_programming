ba1d175b3627097f9803bfac4d9441ed2fbd83ed
Zeddi_Owino <zowino3041@gmail.com>
2022-11-03 19:19:57 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - factorial n
+ * @n: input
+ * Return: Recursion
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
