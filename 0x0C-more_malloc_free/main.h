bb22205288556067ec4e9a1d2af638e0568575d0
Zeddi_Owino <zowino3041@gmail.com>
2022-11-14 01:17:16 -0800

    malloc checked

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,9 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+#include <stdlib.h>
+#include <stdio.h>
+
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
