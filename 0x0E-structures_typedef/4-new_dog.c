84d58cec3429e23b9a014503c947ec8247952bee
Zeddi_Owino <zowino3041@gmail.com>
2022-11-16 03:50:00 -0800

    Free

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -57,5 +57,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_name->name = copy_name;
 	new_name->owner = copy_owner;
 	new_name->age = age;
+	free(new_name);
+	free(copy_name);
+	free(copy_owner);
 	return (new_name);
 }
